93b4a8c1195e99a1239b0260ef0a7f44cb572d6f
dev-mahbub <mahbub.devs@gmail.com>
2025-08-25 21:54:06 +0600

    transtack query wraped provider

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Posts from "./component/posts/posts";
+
+function App() {
+  return (
+    <>
+      <Posts></Posts>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/posts/Posts.tsx
@@ -0,0 +1,10 @@
+const posts = () => {
+  return (
+    <div>
+      <h1>post</h1>
+    </div>
+  );
+};
+
+export default posts;
+<h1>post</h1>;
